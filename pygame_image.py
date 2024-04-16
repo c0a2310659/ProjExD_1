108c02e12cd67306e41949a3c720f37f4816c82c
tera <c0a2310659@edu.teu.ac.jp>
2024-04-16 16:38:21 +0900

    練習7

--- pygame_image.py
+++ pygame_image.py
@@ -9,7 +9,9 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
+
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2=pg.transform.flip(kk_img,True,False)  #練習7
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
     kk_img = pg.transform.rotozoom(kk_img,10,1.0)
@@ -17,8 +19,9 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [-tmr, 0])
+        x=tmr%800
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2,[-x+1600,0])
         screen.blit(kk_img,[300,200])   #練習4
         pg.display.update()
         tmr += 1        
