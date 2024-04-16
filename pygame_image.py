75c668f729c9b8922eab1461efb479760a6954a4
tera <c0a2310659@edu.teu.ac.jp>
2024-04-16 18:09:38 +0900

    演習2

--- pygame_image.py
+++ pygame_image.py
@@ -18,19 +18,22 @@ def main():
     kk_rct.center = 300,200
     tmr = 0
     while True:
+        x=0
+        y=0
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         key_lst = pg.key.get_pressed()
-        if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0,-1))
-        if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0,1))
-        if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((2,0))
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1,0))
-        
+        if key_lst!=False:
+            if key_lst[pg.K_UP]:
+                y=-1
+            if key_lst[pg.K_DOWN]:
+                y=1
+            if key_lst[pg.K_RIGHT]:
+                x=2
+            if key_lst[pg.K_LEFT]:
+                x=-1
+            kk_rct.move_ip((x,y))
         kk_rct.move_ip((-1,0))
 
         x=tmr%3200
